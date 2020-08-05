cb62034391d0daffd68b436a177de1b5f23bf232
Sonu Ahluwalia <sonuahluwalia@hotmail.com>
2020-08-05 19:34:03 +0530

    Update "8/5/2020"

--- spring-boot-modules/spring-boot-data/src/main/java/com/baeldung/boot/jsoncomponent/UserCombinedSerializer.java
+++ spring-boot-modules/spring-boot-data/src/main/java/com/baeldung/boot/jsoncomponent/UserCombinedSerializer.java
@@ -1,5 +1,6 @@
 package com.baeldung.boot.jsoncomponent;
 
+
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
